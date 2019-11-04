b9614e353c5510c8918c5d20157b2eee224f6c85
SylarWesker <vergil047@gmail.com>
2019-11-04 22:41:06 +0300

    Добавил базовые поля (свойства) и пару методов.

--- codebase/Task.php
+++ codebase/Task.php
@@ -1,1 +1,22 @@
 <?php
+
+namespace htmlacademy\taskforce;
+
+class Task
+{
+    protected $id_client; // id заказчика.
+    protected $id_performer; // id исполнителя.
+    protected $finish_date; // срок завершения.
+    protected $status; // текущий статус.
+
+    // Возвращает список статусов. Всех? только названия? Список констант?
+    public function getStatusList() {
+
+    }
+
+    // Возвращает список действий. Всех? только названия? Список констант?
+    public function getActionList() {
+
+    }
+}
+
