5bcb261cbec5538d4bf53858c4efd2d516483743
Sylar <vergil047@gmail.com>
2019-12-14 13:44:42 +0300

    Переписал реализацию Task под обновленный вариант задания.

--- codebase/Task.php
+++ codebase/Task.php
@@ -1,22 +1,128 @@
 <?php
 
+// В этом файле попробую реализовать класс как написано в задании.
 namespace htmlacademy\taskforce;
 
 class Task
 {
-    protected $id_client; // id заказчика.
-    protected $id_performer; // id исполнителя.
-    protected $finish_date; // срок завершения.
-    protected $status; // текущий статус.
+    protected $id_client;       // id заказчика.
+    protected $id_performer;    // id исполнителя.
+    protected $finish_date;     // срок завершения.
+    protected $status;          // текущий статус.
 
-    // Возвращает список статусов. Всех? только названия? Список констант?
-    public function getStatusList() {
+    // Статусы.
+    const STATUS_NEW        = 'new';         // Новое задание (создано Заказчиком)
+    const STATUS_CANCELED   = 'canceled';    // Задание отменено (Заказчиком)
+    const STATUS_WORKED     = 'worked';      // В работе (уставливается после того как Заказчик выбрал откликнувшегося Исполнителя).
+    const STATUS_COMPLETED  = 'completed';   // Задание выполнено (устанавливает Заказчик).
+    const STATUS_FAILED     = 'failed';      // Провалено (если Исполнитель взял в работу и отказался).
 
+    // Действия.
+    const ACTION_CANCEL     = 'cancel';     // Отменить (Заказчик)
+    const ACTION_RESPOND    = 'respond';    // Откликнуться (Исполнитель)
+    const ACTION_REFUSE     = 'refuse';     // Отказаться (Исполнитель)
+    const ACTION_COMPLETE   = 'complete';   // Выполнено (Заказчик)
+    const ACTION_APPOINT    = 'appoint';    // Назначить исполнителя (Заказчик)
+
+    // а как же дата завершения? логичнее передавать id заказчика и дату завершения, а id исполнителя потом установить когда он станет известен.
+    public function __construct($id_client, $id_performer) {
+        $this->id_client = $id_client;
+        $this->id_performer = $id_performer;
+
+        $this->status = self::STATUS_NEW;
     }
 
-    // Возвращает список действий. Всех? только названия? Список констант?
-    public function getActionList() {
+    // Возвращает список статусов.
+    public function getStatusList() {
+        return [
+            self::STATUS_NEW        => 'новое',
+            self::STATUS_CANCELED   => 'отменено',
+            self::STATUS_WORKED     => 'в работе',
+            self::STATUS_COMPLETED  => 'завершено',
+            self::STATUS_FAILED     => 'провалено',
+        ];
+    }
 
+    // Возвращает список действий.
+    public function getActionList() {
+        return [
+            self::ACTION_CANCEL     => 'отменить',
+            self::ACTION_RESPOND    => 'откликнуться',
+            self::ACTION_REFUSE     => 'отказаться',
+            self::ACTION_COMPLETE   => 'выполнено',
+            self::ACTION_APPOINT    => 'назначить исполнителя',
+        ];
+    }
+
+    // ToDo
+    // Возможность применения действия также зависит от роли...
+    // Возвращает статус, в который перейдет задача после указанного действия.
+    public function getNextStatus($action) {
+        $nextStatus = $this->status;
+
+        switch ($action) {
+            case self::ACTION_CANCEL: {
+                if ($this->status == self::STATUS_NEW) {
+                    $nextStatus = self::STATUS_CANCELED;
+                }
+                break;
+            }
+            case self::ACTION_RESPOND: {
+                if ($this->status == self::STATUS_NEW) {
+                    // ToDo
+                    // То что кто-то откликнулся еще не значит, что он сразу становится исполнителем. Исполнителя должен утвердить заказчик.
+                    // т.е получается ничего не меняется???
+                    $nextStatus = $this->status;
+                }
+                break;
+            }
+            case self::ACTION_REFUSE: {
+                if ($this->status == self::STATUS_WORKED) {
+                    $nextStatus = self::STATUS_FAILED;
+                }
+                break;
+            }
+            case self::ACTION_APPOINT: {
+                if ($this->status == self::STATUS_NEW) {
+                    $nextStatus = self::STATUS_WORKED;
+                }
+                break;
+            }
+            case self::ACTION_COMPLETE: {
+                if ($this->status == self::STATUS_WORKED) {
+                    $nextStatus = self::STATUS_COMPLETED;
+                }
+                break;
+            }
+        }
+
+        return $nextStatus;
+    }
+
+    // ToDo
+    // Список действий также зависит от роли...
+    // Возвращает список возможных действий для указанного статуса.
+    public function getActions($status) {
+        $availableActions = [];
+
+        switch ($status) {
+            case self::STATUS_NEW: {
+                $availableActions = [ self::STATUS_CANCELED, self::ACTION_RESPOND ,self::ACTION_APPOINT ];
+                break;
+            }
+            case self::STATUS_WORKED: {
+                $availableActions = [ self::STATUS_FAILED, self::STATUS_COMPLETED ];
+                break;
+            }
+            case self::STATUS_COMPLETED:
+            case self::STATUS_FAILED:
+            case self::STATUS_CANCELED: {
+                // ничего нельзя сделать
+                break;
+            }
+        }
+
+        return $availableActions;
     }
 }
 
