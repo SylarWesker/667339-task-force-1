1ef0f5a24276ff591ac25d7962faa6e5e8f9bd86
SylarWesker <vergil047@gmail.com>
2020-04-23 10:38:19 +0300

    Незначительные правки.

--- src/Utils/Converter.php
+++ src/Utils/Converter.php
@@ -12,8 +12,19 @@ class Converter
     public const ONE_TO_ONE_RELATION = 1;
     public const ONE_TO_MANY_RELATION = 2;
 
+    /**
+     * @var array - ключ имя таблицы, значение - кол-во строк в файле с данными для этой таблицы.
+     */
     private array $tablesRowCount;
+
+    /**
+     * @var int - значение для рандомизатора связей между таблицами.
+     */
     private int $seedForRelations;
+
+    /**
+     * @var string - папка куда будут сохранены sql дампы.
+     */
     private string $sqlSaveFolder;
 
     public function __construct($seedForRelations, $saveFolder = "sql_test_data")
@@ -24,20 +35,6 @@ class Converter
 
     public function work()
     {
-        // Зависимости между данными.
-        //
-        // Населенные пункты - ни от кого не зависит.
-        // Категории - ни от кого не зависит.
-        // Пользователи - зависит от "Населенные пункты", "Категории".
-        // Профили - зависит от "Пользователи".
-        // Задачи - зависит от "Категории", "Населенные пункты", "Пользователи".
-        // Отклики - зависит от "Задачи", "Пользователи".
-        // Отзывы зависит от "Задачи", "Пользователи".
-        //
-        // Нужно выстроить приоритет загрузки данных.
-        // Потом пройтись по всем зависимостям (таблица.поле источника, таблица.поле внешнего ключа, тип зависимости (один к одному, один ко многим)
-        // и создать связи.
-
         $this->tablesRowCount = [];
 
         $filesConfigData = $this->getFileConfigData();
@@ -86,43 +83,6 @@ class Converter
                         $relations_ids[] = rand(1, $dependencyTableRowsCount);
                     }
 
-                    /*
-                    switch ($table) {
-                        case 'category':
-                        case 'locality':
-                        case 'user':
-                        case 'task_status': {
-                            // могут повторяться id.
-                            for ($counter = 0; $counter < $sourceTableRowsCount; $counter++) {
-                                $relations_ids[] = rand(1, $dependencyTableRowsCount);
-                            }
-                            break;
-                        }
-                        case 'profile': {
-                            // Не должны повторяться id.
-
-                            $all_ids = range(1, $dependencyTableRowsCount); // Генерируем все возможные id.
-
-                            for ($counter = 0; $counter < $sourceTableRowsCount; $counter++) {
-                                $index = rand(0, count($all_ids) - 1);
-                                $id = $all_ids[$index];
-
-                                $relations_ids[] = $id;
-
-                                array_splice($all_ids, $index, 1);
-                                // или еще вариант. не знаю что быстрее работает.
-                                // unset($all_ids[$index]);
-                                // $all_ids = array_values($all_ids);
-                            }
-                            break;
-                        }
-                        case 'task': {
-                            // тут самое сложное.
-                            // в зависимости от поля меняется правило.
-                        }
-                    }
-                    */
-
                     $dependency_data[$source_table_field] = $relations_ids;
                 }
             }
@@ -136,88 +96,13 @@ class Converter
             $saveFilePath = join(DIRECTORY_SEPARATOR, [$_SERVER['DOCUMENT_ROOT'], $fileName]);
             SqlFileWriter::save($insert_sql, $saveFilePath);
         }
-
-        $relations_sql = [];
-
-        /*foreach ($relations as $relation) {
-            $dependencies = $relation['dependencies'];
-            $table = $relation['table'];
-
-            // ToDo
-            // Следует ли передавать в качестве параметра метода поле этого класса.
-            if (!empty($dependencies) || !is_null($dependencies)) {
-                $relations_sql[] = $this->generateRelations($table, $dependencies, $this->seedForRelations);
-            }
-        }
-
-        $all_relations_sql = join(PHP_EOL, $relations_sql);
-        $relationsFileName = "$this->sqlSaveFolder/relations.sql";
-        SqlFileWriter::save($all_relations_sql, $relationsFileName);*/
-    }
-
-    /**
-     * Создает sql код взаимосвязей данных между таблицами (прописывает значение внешних ключей с помощью UPDATE).
-     *
-     * @param string $table
-     * @param array $dependencies
-     * @param int $seed
-     *
-     * @return string
-     */
-    private function generateRelations(string $table, array $dependencies, int $seed): string
-    {
-        $sql = [];
-        $source_table_rows_count = $this->tablesRowCount[$table];
-        srand($seed);
-
-        for ($row_counter = 1; $row_counter <= $source_table_rows_count; $row_counter++) {
-            $set_sql = [];
-
-            foreach ($dependencies as $dependency) {
-                $foreign_table = $dependency['foreign_table'];
-                $foreign_table_rows_count = $this->tablesRowCount[$foreign_table];
-
-                $field = $dependency['source_table_field'];
-                $foreign_key_value = rand(1, $foreign_table_rows_count);
-
-                $set_sql[] = "$field = $foreign_key_value";
-            }
-
-            $field_value = join(', ', $set_sql);
-            $sql[] = "UPDATE $table SET $field_value WHERE id = $row_counter;";
-
-            //$rel_type = $dependency['relation_type'];
-
-            // Генерация значений внешних ключей.
-            /* $foreign_key_values = [];
-            if ($rel_type == TEST::ONE_TO_MANY_RELATION) {
-            $min_rel_count = $dependency['min_rel_count'];
-            $max_rel_count = $dependency['max_rel_count'];
-
-            $rel_count = rand($min_rel_count, $max_rel_count);
-            $all_foreign_ids = range(1, $foreign_table_rows_count);
-
-            for($rel_counter = 0; $rel_counter < $rel_count; $rel_counter++) {
-            $index = rand(1, count($all_foreign_ids));
-            $foreign_key_values[] = $all_foreign_ids[$index];
-
-            $all_foreign_ids = array_slice($all_foreign_ids, $index,1);
-            }
-
-            } elseif ($rel_type == TEST::ONE_TO_ONE_RELATION) {
-            $foreign_key_values[] = rand(1, $foreign_table_rows_count);
-            }*/
-        }
-
-        $result = join(PHP_EOL, $sql);
-        return $result;
     }
 
     /**
      *  В зависимости от данных переданных в конвертер выставляем режим чтения файла.
      *
-     * @param array $columnsData
-     * @return array
+     * @param array $columnsData - данные о колонках в файле.
+     * @return array - настроечные параметры для класса чтения данных из файла.
      */
     private function getReaderParams($columnsData)
     {
@@ -363,7 +248,6 @@ class Converter
                 ['foreign_table' => 'user', 'source_table_field' => 'client_id', 'relation_type' => Converter::ONE_TO_ONE_RELATION],
                 ['foreign_table' => 'user', 'source_table_field' => 'performer_id', 'relation_type' => Converter::ONE_TO_ONE_RELATION],
                 ['foreign_table' => 'task_status', 'source_table_field' => 'status_id', 'relation_type' => Converter::ONE_TO_ONE_RELATION],
-                ['foreign_table' => 'category', 'source_table_field' => 'category_id', 'relation_type' => Converter::ONE_TO_ONE_RELATION],
                 ['foreign_table' => 'locality', 'source_table_field' => 'locality_id', 'relation_type' => Converter::ONE_TO_ONE_RELATION],
             ]],
             'response' => ['dependencies' => [
--- src/Utils/SqlFileWriter.php
+++ src/Utils/SqlFileWriter.php
@@ -10,18 +10,24 @@ namespace TaskForce\Utils;
 class SqlFileWriter
 {
     /**
+     * Метод сохранения строки с sql кодом в файл.
+     *
      * @param string $sql - строка с sql кодом.
      * @param string $filePath - путь к файлу, куда сохраняем sql код.
+     *
+     * @return int - результат сохранения.
      */
-    public static function save(string $sql, string $filePath)
+    public static function save(string $sql, string $filePath): int
     {
         $file_directory = dirname($filePath);
-        if(!is_dir($file_directory)) {
+        if (!is_dir($file_directory)) {
             mkdir($file_directory);
         }
 
         $fileWriter = new \SplFileObject($filePath, 'w');
         $writeResult = $fileWriter->fwrite($sql);
         $fileWriter = null;
+
+        return $writeResult;
     }
 }
